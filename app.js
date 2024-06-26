465eafc3b60cedc548bf81469b9c6619f31439de
Gzenxs <gzenxs@gmail.com>
2024-06-26 16:00:37 -0600

    Template Strings

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+//Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor");
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor: ");
 
+console.log(numeroUsuario);
+/*
+Este codigo realiza la comparacion.
+*/
 if (numeroUsuario == numeroSecreto) {
-    alert("Acertaste el número");
+    //Acertamos, fue verdadera la condición
+    alert(`Acertaste el número secreto es ${numeroSecreto}`);
+} else {
+    //Lacondicion no se cumplió
+    alert("Lo siento, no acertaste el número");
 }
