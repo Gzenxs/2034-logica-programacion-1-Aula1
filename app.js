778e893fe290bb4c444cdabedebf68a6576f272b
Gzenxs <gzenxs@gmail.com>
2024-06-26 15:23:58 -0600

    Dos

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número por favor");
-
-if (numeroUsuario == numeroSecreto) {
-    alert("Acertaste el número");
-}
+//Declara una variable llamada nombre y asígnale el valor "Lua".
+let nombre = "Lua";
