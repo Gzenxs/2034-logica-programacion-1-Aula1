18fd3ae0f15de04e3525e7f6e3c54314298ca110
Gzenxs <gzenxs@gmail.com>
2024-06-26 15:46:21 -0600

    Dies

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-//Pide al usuario que ingrese su edad usando un prompt y almacénala en la variable edad.
+//Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puede obtener su licencia de conducir!".
 let edad = prompt('Digite su edad');
+if (edad >= 18) {
+    alert('¡Puede obtener su licencia de conducir!');
+}
