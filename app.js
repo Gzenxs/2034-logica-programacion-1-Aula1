5bbe8131a6d3506a6be3577dcbdb1714bcbcf555
Gzenxs <gzenxs@gmail.com>
2024-06-28 11:52:38 -0600

    Loops y Bucles

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 //Variables
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor: ");
+let numeroUsuario = 0;
+while (numeroUsuario != numeroSecreto) {
 
-console.log(numeroUsuario);
-/*
-Este codigo realiza la comparacion.
-*/
-if (numeroUsuario == numeroSecreto) {
-    //Acertamos, fue verdadera la condición
-    alert(`Acertaste el número secreto es ${numeroSecreto}`);
-} else {
-    if (numeroUsuario > numeroSecreto) {
-        alert("El numero Secreto es Menor");
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor: ");
+    /*
+    Este codigo realiza la comparacion.
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste el número secreto es ${numeroSecreto}`);
     } else {
-        alert("El numero Secreto es Mayor");
-    }
+        if (numeroUsuario > numeroSecreto) {
+            alert("El numero Secreto es Menor");
+        } else {
+            alert("El numero Secreto es Mayor");
+        }
+    }    
 }
