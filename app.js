f37c63226304f7ec1bda2be71c9a659ce735b8e2
Gzenxs <gzenxs@gmail.com>
2024-06-28 12:29:13 -0600

    Dos

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-//Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+//Crea un contador que comience en 10 y vaya hasta 0 usando un bucle 'while'. Muestra cada número.
 
-let contador = 1;
-while (contador <= 10) {
+let contador = 10;
+while (contador >= 0) {
 alert(contador);
-contador++;
+contador--;
 }
