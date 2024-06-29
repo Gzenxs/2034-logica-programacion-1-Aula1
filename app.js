9845c6fb5b47cb85f52a5f470cf4be37a73c7996
Gzenxs <gzenxs@gmail.com>
2024-06-29 14:47:44 -0600

    Dies

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-//Utiliza un bucle while para mostrar los números del 1 al 10 en la consola.
+//Crea una variable "nota" y asígnale un valor numérico. Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
 
-numero = 1;
-   while (numero <= 10) {
-       console.log(numero);
-       numero++;
-   }
+nota = Math.floor(Math.random()*11);
+console.log(nota); 
+if (nota >= 7) {
+    console.log("Aprobado");
+} else {
+    console.log("Reprobado");
+}
