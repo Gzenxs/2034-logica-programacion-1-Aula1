448b4fc9bd1b42664eb65ce6d6f420dca5f2373c
Gzenxs <gzenxs@gmail.com>
2024-06-29 14:25:57 -0600

    Cinco

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-//Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
-
-lenguajePreferido = prompt('¿Cuál es el lenguaje de programación que más te gusta?');
-   console.log(lenguajePreferido);
+//Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+let valor1 = 40;
+let valor2 = 4;
+let resultado = valor1 + valor2;
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`);
