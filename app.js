617d586764853776bd25f1c6b2e6657629caa428
Gzenxs <gzenxs@gmail.com>
2024-06-29 14:11:53 -0600

    Cuatro

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza alert para mostrar el mensaje "¡Hola, [tu nombre]!".
+//Utiliza prompt y haz la siguiente pregunta: ¿Cuál es el lenguaje de programación que más te gusta?. Luego, almacena la respuesta en una variable y muestra la respuesta en la consola del navegador.
 
-nombre = 'Gzenxs';
-alert(`¡Hola, ${nombre}!`);
+lenguajePreferido = prompt('¿Cuál es el lenguaje de programación que más te gusta?');
+   console.log(lenguajePreferido);
