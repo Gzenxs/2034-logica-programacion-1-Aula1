85ae9fa05791c7c24acd5f647f51cb253423bc47
Gzenxs <gzenxs@gmail.com>
2024-06-29 14:08:49 -0600

    Dos

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
-//Crea un programa que utilice console.log para mostrar un mensaje de bienvenida.
-console.log('¡Bienvenido!');
+//Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!" en la consola del navegador.
+
+nombre = 'Gzenxs';
+console.log(`¡Hola, ${nombre}!`);
